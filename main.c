1108266f510224f483c8eadcf94e49bceb51ca34
Nathan Alex Sequeira <nathanseq12@gmail.com>
2025-12-03 11:21:47 -0700

    Splash page created based on ratios, but default set to 30x80

--- main.c
+++ main.c
@@ -1,37 +1,59 @@
+//#define "levels.h"
+#include <stdio.h>
+#include <ncurses.h>
+#include <string.h>
+
+#define MAXY 30
+#define MAXX 80
+
 void readFile(FILE *f)
 {
-        int y, x, s;
-        char buffer;
-        while (fscanf(f, "%d, %d, %d\n", &y, &x, &s) == 3)
+        float yf, xf, sf;
+        int yi, xi, si;
+        while (fscanf(f, "%f, %f, %f\n", &yf, &xf, &sf) == 3)
         {
-                mvhline(y, x, ACS_HLINE, s);
+                yi = yf * MAXY;
+                xi = xf * MAXX;
+                si = sf * MAXX;
+                mvhline(yi, xi, ACS_HLINE, si);
         }
-        buffer = fgetc(f); // Consume line between coordinate sets.
-        while (fscanf(f, "%d, %d, %d\n", &y, &x, &s) == 3)
+        fgetc(f); // Consume line between coordinate sets.
+        while (fscanf(f, "%f, %f, %f\n", &yf, &xf, &sf) == 3)
         {
-                mvvline(y, x, ACS_VLINE, s);
+                yi = yf * MAXY;
+                xi = xf * MAXX;
+                si = sf * MAXY;
+                mvvline(yi, xi, ACS_VLINE, si);
         }
-        buffer = fgetc(f);
-        while (fscanf(f, "%d, %d\n", &y, &x) == 2)
+        fgetc(f);
+        while (fscanf(f, "%f, %f\n", &yf, &xf) == 2)
         {
-                mvaddch(y, x, ACS_ULCORNER);
+                yi = yf * MAXY;
+                xi = xf * MAXX;
+                mvaddch(yi, xi, ACS_ULCORNER);
         }
-        buffer = fgetc(f);
-        while (fscanf(f, "%d, %d\n", &y, &x) == 2)
+        fgetc(f);
+        while (fscanf(f, "%f, %f\n", &yf, &xf) == 2)
         {
-                mvaddch(y, x, ACS_LLCORNER);
+                yi = yf * MAXY;
+                xi = xf * MAXX;
+                mvaddch(yi, xi, ACS_LLCORNER);
         }
-        buffer = fgetc(f);
-        while (fscanf(f, "%d, %d\n", &y, &x) == 2)
+        fgetc(f);
+        while (fscanf(f, "%f, %f\n", &yf, &xf) == 2)
         {
-                mvaddch(y, x, ACS_LRCORNER);
+                yi = yf * MAXY;
+                xi = xf * MAXX;
+                mvaddch(yi, xi, ACS_LRCORNER);
         }
-        buffer = fgetc(f);
-        while (fscanf(f, "%d, %d\n", &y, &x) == 2)
+        fgetc(f);
+        while (fscanf(f, "%f, %f\n", &yf, &xf) == 2)
         {
-                mvaddch(y, x, ACS_URCORNER);
+                yi = yf * MAXY;
+                xi = xf * MAXX;
+                mvaddch(yi, xi, ACS_URCORNER);
         }
-        buffer = fgetc(f);
+        fgetc(f);
 }
 
 void splashPage(WINDOW *w)
@@ -58,24 +80,45 @@ void splashPage(WINDOW *w)
         fclose(f);
         attron(COLOR_PAIR(6));
         // -
-        mvaddch(6, 38, ACS_ULCORNER);
-        mvaddch(7, 38, ACS_LLCORNER);
-        mvhline(7, 39, ACS_HLINE, 3);
-        mvaddch(7, 42, ACS_LRCORNER);
-        mvaddch(6, 42, ACS_URCORNER);
-        mvhline(6, 39, ACS_HLINE, 3);
+        mvaddch((int)(0.2*MAXY), (int)(0.475*MAXX), ACS_ULCORNER);
+        mvaddch((int)(0.234*MAXY), (int)(0.475*MAXX), ACS_LLCORNER);
+        mvhline((int)(0.234*MAXY), (int)(0.4875*MAXX), ACS_HLINE, (int)(0.0375*MAXX));
+        mvaddch((int)(0.234*MAXY), (int)(0.525*MAXX), ACS_LRCORNER);
+        mvaddch((int)(0.2*MAXY), (int)(0.525*MAXX), ACS_URCORNER);
+        mvhline((int)(0.2*MAXY), (int)(0.4875*MAXX), ACS_HLINE, (int)(0.0375*MAXX));
         attron(COLOR_PAIR(5));
-        mvprintw(11, 2, "                            (not Pac-Man)                                     ");
-        mvprintw(14, 2, "                     Nathan Alex Sequeira (3141620)                           ");
-        mvprintw(15, 2, "                         Nima Houshyar (xxxxxxx)                              ");
+        char *message[] = {"(not Pac-Man)", "Nathan Alex Sequeira (3141620)", "Nima Houshyar (xxxxxxx)", "Controls", "Use <arrows> to move player", "Press <space> to activate portal", "After collecting a powerup, press <1> or <2> to freeze half of the enemies", "Press <E> to start easy", "Press <H> to start hard"};
+        int y, x;
+        attron(COLOR_PAIR(5));
+        y = 0.367 * MAXY;
+        x = (MAXX/2) - (strlen(message[0])/2);
+        mvprintw(y, x, "%s", message[0]);
+        y = 0.467 * MAXY;
+        x = (MAXX/2) - (strlen(message[1])/2);
+        mvprintw(y, x, "%s", message[1]);
+        y = 0.5 * MAXY;
+        x = (MAXX/2) - (strlen(message[2])/2);
+        mvprintw(y, x, "%s", message[2]);
         attron(COLOR_PAIR(2));
-        mvprintw(19, 2, "                              Controls                                        ");
-        mvprintw(20, 2, "                      Use <arrows> to move player                             ");
-        mvprintw(21, 2, "                  Press <space> to activate teleporter                        ");
-        mvprintw(22, 2, "  After collecting a powerup, press <1> or <2> to freeze half of the enemies  ");
+        y = 0.634 * MAXY;
+        x = (MAXX/2) - (strlen(message[3])/2);
+        mvprintw(y, x, "%s", message[3]);
+        y = 0.67 * MAXY;
+        x = (MAXX/2) - (strlen(message[4])/2);
+        mvprintw(y, x, "%s", message[4]);
+        y = 0.7 * MAXY;
+        x = (MAXX/2) - (strlen(message[5])/2);
+        mvprintw(y, x, "%s", message[5]);
+        y = 0.734 * MAXY;
+        x = (MAXX/2) - (strlen(message[6])/2);
+        mvprintw(y, x, "%s", message[6]);
         attron(COLOR_PAIR(7));
-        mvprintw(26, 2, "                        Press <E> to start easy.                              ");
-        mvprintw(27, 2, "                        Press <H> to start hard.                              ");
+        y = 0.867 * MAXY;
+        x = (MAXX/2) - (strlen(message[7])/2);
+        mvprintw(y, x, "%s", message[7]);
+        y = 0.9 * MAXY;
+        x = (MAXX/2) - (strlen(message[8])/2);
+        mvprintw(y, x, "%s", message[8]);
         wrefresh(w);
         refresh();
 }
@@ -86,7 +129,7 @@ int main(void)
         initscr(); // Initialize window
         noecho(); // Entered keys are not printed on the screen.
         curs_set(FALSE); // Don't display a cursor
-        w = newwin(30, 80, 1, 1); // Window size
+        w = newwin(MAXY, MAXX, 1, 1); // Window size
         box(w, 0, 0); // Default border to window.
         wrefresh(w);
         refresh();
