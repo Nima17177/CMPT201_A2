bbb290854349cd872161275a5796d161a71ad36d
Nathan Alex Sequeira <nathanseq12@gmail.com>
2025-12-04 13:02:41 -0700

    gfcyhuij

--- map.c
+++ map.c
@@ -7,20 +7,20 @@
 void createMapEasy(WINDOW *w)
 {
     //wborder(w, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
-    
+
     /*int top_y = MAP_Y /4;
     int buttom_y = MAP_Y *3/4;
     int left_x = MAP_X /4;
     int right_x = MAP_X *3/4;
     \'-'
-    
-   
+
+
     mvwaddch(w, top_y, left_x, ACS_ULCORNER); // left top corner
     mvwaddch(w, top_y, right_x, ACS_URCORNER); // right top corner
     mvwaddch(w, buttom_y, left_x, ACS_LLCORNER); // left bottom corner
-    mvwaddch(w, buttom_y, right_x, ACS_LRCORNER); // right bottom corner 
+    mvwaddch(w, buttom_y, right_x, ACS_LRCORNER); // right bottom corner
     */
-    char line[45];
+    /*char line[45];
         int x = 1;
         int y = 1;
         FILE *f = fopen("EasyMap.txt", "r");
@@ -30,7 +30,27 @@ void createMapEasy(WINDOW *w)
                 mvwprintw(w, y++, x, "%s", line);
                 fgetc(f);
         }
-        fclose(f);
+        fclose(f);*/
+         mvprintw(1, 1, "+-----------------------------------------+");
+         mvprintw(2, 1, "|     |    |    |         |    |    |     |");
+         mvprintw(3, 1, "|     |   ---   |   ---   |   ---   |     |");
+         mvprintw(4, 1, "|   |---|     |---|     |---|     |---|   |");
+         mvprintw(5, 1, "|-|       |-|       |-|       |-|       |-|");
+         mvprintw(6, 1, "|   |---|     |---|     |---|     |---|   |");
+         mvprintw(7, 1, "|-|       |-|       |-|       |-|       |-|");
+         mvprintw(8, 1, "|   |---|     |---|     |---|     |---|   |");
+         mvprintw(9, 1, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(10, 1, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(11, 1, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(12, 1, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(13, 1, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(14, 1, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(15, 1, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(16, 1, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(17, 1, "|     |   ---   |         |   ---   |     |");
+        mvprintw(18, 1, "|     |    |    |         |    |    |     |");
+        mvprintw(19, 1, "+-----------------------------------------+");
+
         wrefresh(w);
     refresh();
 }
