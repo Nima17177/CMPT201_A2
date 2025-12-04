07bde396e1f8e1157fb2c9c1cc8d692db06da5b7
Nima <nimahoushyar@gmail.com>
2025-12-04 11:50:13 -0700

    CreatedMazeE

---
+++ map.c
@@ -0,0 +1,35 @@
+#include <ncurses.h>
+#include "map.h"
+
+#define MAP_X 80
+#define MAP_Y 30
+
+void createMapEasy(WINDOW *w)
+{
+    wborder(w, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+    
+    int top_y = MAP_Y /4;
+    int buttom_y = MAP_Y *3/4;
+    int left_x = MAP_X /4;
+    int right_x = MAP_X *3/4;
+    \'-'
+    
+    /*
+    mvwaddch(w, top_y, left_x, ACS_ULCORNER); // left top corner
+    mvwaddch(w, top_y, right_x, ACS_URCORNER); // right top corner
+    mvwaddch(w, buttom_y, left_x, ACS_LLCORNER); // left bottom corner
+    mvwaddch(w, buttom_y, right_x, ACS_LRCORNER); // right bottom corner 
+    */
+    char line[44];
+    int x = 1;
+    int y = 1;
+    FILE *f = fopen("EasyMaze.txt", "r");
+    for (int i=0; i<19; i++) {
+        fscanf(f, "%s\n", line);
+        mvwprintw(w, y++, x, "%s", line);
+    }
+    
+    fclose(f);
+
+    wrefresh(w);
+}
