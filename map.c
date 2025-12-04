01420d0871cdb636d91508e1a70f5de671965fad
Nima <nimahoushyar@gmail.com>
2025-12-04 14:19:54 -0700

    Update map.c

--- map.c
+++ map.c
@@ -7,49 +7,30 @@
 void createMapEasy(WINDOW *w)
 {
     wborder(w, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
-
-    /*int top_y = MAP_Y /4;
-    int buttom_y = MAP_Y *3/4;
-    int left_x = MAP_X /4;
-    int right_x = MAP_X *3/4;
-    \'-'
-
-
-    mvwaddch(w, top_y, left_x, ACS_ULCORNER); // left top corner
-    mvwaddch(w, top_y, right_x, ACS_URCORNER); // right top corner
-    mvwaddch(w, buttom_y, left_x, ACS_LLCORNER); // left bottom corner
-    mvwaddch(w, buttom_y, right_x, ACS_LRCORNER); // right bottom corner
-    */
-    /*char line[45];
-        int x = 1;
-        int y = 1;
-        FILE *f = fopen("EasyMap.txt", "r");
-        for (int i = 0; i < 19; i++)
-        {
-                fgets(line, 44, f);
-                mvwprintw(w, y++, x, "%s", line);
-                fgetc(f);
-        }
-        fclose(f);*/
-         mvprintw(1, 1, "+-----------------------------------------+");
-         mvprintw(2, 1, "|     |    |    |         |    |    |     |");
-         mvprintw(3, 1, "|     |   ---   |   ---   |   ---   |     |");
-         mvprintw(4, 1, "|   |---|     |---|     |---|     |---|   |");
-         mvprintw(5, 1, "|-|       |-|       |-|       |-|       |-|");
-         mvprintw(6, 1, "|   |---|     |---|     |---|     |---|   |");
-         mvprintw(7, 1, "|-|       |-|       |-|       |-|       |-|");
-         mvprintw(8, 1, "|   |---|     |---|     |---|     |---|   |");
-         mvprintw(9, 1, "|-|       |-|       |-|       |-|       |-|");
-        mvprintw(10, 1, "|   |---|     |---|     |---|     |---|   |");
-        mvprintw(11, 1, "|-|       |-|       |-|       |-|       |-|");
-        mvprintw(12, 1, "|   |---|     |---|     |---|     |---|   |");
-        mvprintw(13, 1, "|-|       |-|       |-|       |-|       |-|");
-        mvprintw(14, 1, "|   |---|     |---|     |---|     |---|   |");
-        mvprintw(15, 1, "|-|       |-|       |-|       |-|       |-|");
-        mvprintw(16, 1, "|   |---|     |---|     |---|     |---|   |");
-        mvprintw(17, 1, "|     |   ---   |         |   ---   |     |");
-        mvprintw(18, 1, "|     |    |    |         |    |    |     |");
-        mvprintw(19, 1, "+-----------------------------------------+");
+    // Title
+    mvprintw(2, (MAP_X / 2) - 5, "PAC-MAN");
+    
+    // Maze layout for easy mode
+         mvprintw(7, 20, "+-----------------------------------------+");
+         mvprintw(8, 20, "|     |    |    |         |    |    |     |");
+         mvprintw(9, 20, "|     |   ---   |   ---   |   ---   |     |");
+        mvprintw(10, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(11, 20, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(12, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(13, 20, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(14, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(15, 20, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(16, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(17, 20, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(18, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(19, 20, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(20, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(21, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(22, 20, "|-|       |-|       |-|       |-|       |-|");
+        mvprintw(23, 20, "|   |---|     |---|     |---|     |---|   |");
+        mvprintw(24, 20, "|     |   ---   |         |   ---   |     |");
+        mvprintw(25, 20, "|     |    |    |         |    |    |     |");
+        mvprintw(26, 20, "+-----------------------------------------+");
 
         wrefresh(w);
     refresh();
